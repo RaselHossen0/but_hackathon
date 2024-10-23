54ac25434b8d21ecee73a800f91a68babb1a57ec
Md. Rasel Hossen <hrasel2002@gmail.com>
2024-10-23 17:44:57 +0600

    Update server URL in swagger.js

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'https://nofear-backend.starstechgroup.com:3000',
+        url: 'https://nofear-backend.starstechgroup.com',
       },
     ],
   },
