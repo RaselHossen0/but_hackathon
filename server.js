302a20f5546505838fe66b69bdd0df3c00ad7cff
Md. Rasel Hossen <hrasel2002@gmail.com>
2024-10-23 16:45:58 +0600

    Update database and server configuration

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const swaggerSetup = require('./swagger');
 const User = require('./models/User');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
